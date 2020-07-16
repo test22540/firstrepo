b2bea6e654fc616fe4ebcf6f37ebeb660f6efe3e
test22540 <67623513+test22540@users.noreply.github.com>
2020-07-16 10:31:04 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,5 +1,18 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+
+class MisspelledVar { 
+    public static void main(String args[]) 
+    { 
+        int a = 40, b = 0;
+        int c=10
+  
+        // Declared variable Sum with Capital S 
+        int Sum = a + b; 
+        int d = a/b;
+        // Trying to call variable Sum 
+        // with a small s ie. sum 
+        System.out.println( 
+            "Sum of variables is "
+            + sum); 
+    } 
+} 
+ 
