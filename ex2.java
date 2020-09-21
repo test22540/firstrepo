72b068bd0e6b7bf1acc4f2a55db96e10d1224c8d
test22540 <67623513+test22540@users.noreply.github.com>
2020-09-21 11:20:43 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -6,7 +6,7 @@ class MisspelledVar {
         int c=10
   
         // Declared variable Sum with Capital S 
-        int Sum = a + b; 
+        int sum = a + b; 
         int d = a/b;
         // Trying to call variable Sum 
         // with a small s ie. sum 
